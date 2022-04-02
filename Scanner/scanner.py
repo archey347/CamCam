b9a33639bffd1e918aecdd79ae302b11a2c0bf87
Archey Barrell <archey.barrell@outlook.com>
2022-04-02 21:17:25 +0100

    Scanner stuff

--- Scanner/scanner.py
+++ Scanner/scanner.py
@@ -1,71 +1,67 @@
-import os
-import subprocess
+#!/usr/bin/python3
+import iw
 import sys
-import getopt
-import json
 import time
+import json
+import requests
 
-class Observation:
-	def __init__(self, location, stations) -> None:
-		self.location = location;
-		self.stations = stations;
+# scanner.py <net interface> <location> <n> <sleep>
 
-def printUsage():
-	print("scanner.py -l <location> -n <network>")
+if len(sys.argv) != 6:
+    print("Usage: scanner.py <net interface> <location> <n> <sleep> <ssid>")
+    sys.exit(1)
 
-def scan():
-	os.system("wpa_cli scan")
-	return subprocess.check_output("wpa_cli scan_result")
+net = sys.argv[1]
+location = sys.argv[2]
+n = int(sys.argv[3])
+sleep = int(sys.argv[4])
+ssid = sys.argv[5]
 
-def getStations(fileContents):
-	stations = []
-	for line in fileContents:
-		line = line.split('\t')
+observations = {}
 
-		bssid = line[0]
-		signalLevel = line[2]
-		ssid = line[4].replace("\n", "")
+def upload(data):
+    url = "http://127.0.0.1/camcam/?key=JayDoesn'tKnowWhatATomTomIs"
+    myobj = {'data': data}
 
-		bssidInt = int(bssid.replace(":", ""), 16)
+    x = requests.post(url, data = myobj)
 
-		if (ssid == targetSsid):
-			station = [bssidInt, signalLevel]
-			stations.append(station)
-
-		return stations
+    print(x.text)
 
 def sendObservation(observation):
-	pass
-
-targetSsid = "eduroam"
-scanDuration = 5
-
-if __name__ == "__main__":
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hdl:n:", ["duration=", "location=", "network="])
-	except getopt.GetoptError:
-		printUsage()
-		sys.exit(1)
-
-	for opt, arg in opts:
-		if (opt == "-h"):
-			printUsage();
-			sys.exit()
-		elif (opt in ("-l", "--location")):
-			location = arg
-		elif (opt in ("-n", "--network")):
-			targetSsid = arg
-		elif (opt in ("-d", "--duration")):
-			scanDuration = arg
-
+	#data = json.dumps({
+    #		"location": observation.location,
+	#	"stations": observation.stations
+	#})
 	
-	end = time.time() + scanDuration
-	while (time.time() < end):
-		scanResult = scan()
-		stations = getStations(scanResult)
+	#print(data, end="\n\n")
+	upload(observation)
 
-	if (len(stations) > 0):
-		observation = Observation(location, stations)
+for i in range(0, n):
+    print("Scanning...", i, end="\r")
+    networks = iw.get_interfaces(interface=net)
 
-	sendObservation(observation)
-	
+    observation = {
+        "location": location,
+        "stations": []
+    }
+
+    for station in networks:
+        if station["Name"] == ssid:
+
+            observation["stations"].append([
+                station["Address"], 
+                station["Signal Level"]
+            ])
+
+ 
+
+    sendObservation(observation)
+
+
+    time.sleep(sleep)
+
+
+
+
+
+
