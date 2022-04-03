30c2f3662f89347b437aefa63743371a97d5999a
Jay Rabjohns <52382074+jayrabjohns@users.noreply.github.com>
2022-04-03 02:41:24 +0100

    Added search bar with no functionality

--- App/app/src/main/java/com/example/camcam/ui/search/SearchFragment.java
+++ App/app/src/main/java/com/example/camcam/ui/search/SearchFragment.java
@@ -8,14 +8,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.camcam.R;
+import com.example.camcam.databinding.FragmentHomeBinding;
+import com.example.camcam.databinding.SearchFragmentBinding;
+
+import java.util.Locale;
 
 public class SearchFragment extends Fragment {
 
+    private SearchFragmentBinding binding;
     private SearchViewModel mViewModel;
 
     public static SearchFragment newInstance() {
@@ -25,7 +32,31 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        binding = SearchFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.searchBar.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after)
+            { return; }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count)
+            { return; }
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                if (editable.toString().toLowerCase().contains("bath"))
+                {
+                    // Show bath model
+                }
+            }
+        });
+
+        return root;
+        // return inflater.inflate(R.layout.search_fragment, container, false);
     }
 
     @Override
