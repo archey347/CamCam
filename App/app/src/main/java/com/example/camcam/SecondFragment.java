05f0ac3b2db465172e7a96cda995f154c32acb65
Archey Barrell <archey.barrell@outlook.com>
2022-04-03 03:11:29 +0100

    WiFi Scanning works :)

--- App/app/src/main/java/com/example/camcam/SecondFragment.java
+++ App/app/src/main/java/com/example/camcam/SecondFragment.java
@@ -1,15 +1,20 @@
 package com.example.camcam;
 
+import android.net.wifi.ScanResult;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.camcam.databinding.FragmentSecondBinding;
+
+import java.util.List;
 
 public class SecondFragment extends Fragment {
 
@@ -34,6 +39,25 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        ((Button)  getView().findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WiFi wifi = new WiFi(getContext(), new WiFi.WiFiReciever() {
+                    @Override
+                    public void handleSuccess(List<ScanResult> list) {
+                        ((TextView) getView().findViewById(R.id.textView)).setText(list.get(0).SSID);
+                    }
+
+                    @Override
+                    public void handleFailure() {
+                        ((TextView) getView().findViewById(R.id.textView)).setText("FAIL");
+                    }
+                });
+
+                wifi.scan();
             }
         });
     }
---
+++ App/app/src/main/java/com/example/camcam/WiFi.java
@@ -0,0 +1,72 @@
+package com.example.camcam;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import java.util.List;
+
+public class WiFi {
+
+    Context appContext;
+    WifiManager wifiManager;
+
+    public interface WiFiReciever {
+        public void handleSuccess(List<ScanResult> list);
+
+        public void handleFailure();
+    }
+
+    public WiFiReciever reciever;
+
+
+    public WiFi(Context appContext, WiFiReciever reciever) {
+        this.wifiManager = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE);
+        this.reciever = reciever;
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    scanSuccess();
+                } else {
+                    // scan failure handling
+                    scanFailure();
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        appContext.registerReceiver(wifiScanReceiver, intentFilter);
+    }
+
+    public void scan() {
+        boolean success = wifiManager.startScan();
+        if (!success) {
+            // scan failure handling
+            scanFailure();
+        }
+    }
+
+    public void scanSuccess() {
+        List<ScanResult> results = wifiManager.getScanResults();
+
+        this.reciever.handleSuccess(results);
+    }
+
+    public void scanFailure() {
+        List<ScanResult> results = wifiManager.getScanResults();
+
+
+        this.reciever.handleFailure();
+    }
+
+
+
+}
